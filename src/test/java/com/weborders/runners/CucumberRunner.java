558a7da05617dbb3df9daf1c2cf78d1377f6eb36
abraham <abrahamisaachope@gmail.com>
2020-04-22 20:40:28 -0500

    framework created

---
+++ src/test/java/com/weborders/runners/CucumberRunner.java
@@ -0,0 +1,20 @@
+package com.weborders.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue="com/weborders/step_definitions",
+        features= "src/test/resources/features",
+        tags = "",
+        dryRun = false,
+        plugin = "html:target/default-report"
+
+
+)
+
+
+public class CucumberRunner {
+}
